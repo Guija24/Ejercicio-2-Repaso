c72b429139c6f693ca921030362f23f5a4095abd
Ivan Guijarro <93186068+Guija24@users.noreply.github.com>
2022-11-16 14:10:29 +0100

    Add files via upload

---
+++ ej4.py
@@ -0,0 +1,6 @@
+base=float(input('Dime la base del rectangulo: '))
+altura=float(input('Dimela altura del rectangulo: '))
+area=base*altura
+perimetro=2*base+2*altura
+print(f'El perimetro del rectangulo es {area}')
+print(f'El area del rectangulo es {perimetro}')
